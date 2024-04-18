0c5522ecb8d5bf3dce049c8bd31773dfd384746f
Jonatas Viana <jonatas.viana@kria.com.br>
2024-04-18 17:25:00 -0300

    18 a 20

---
+++ Seção 2 - JavaScript Básico - Para iniciantes/18. Let vs Var - Primeira diferença.js
@@ -0,0 +1,8 @@
+//Deixa declarar variavel novamente
+var nome = "Eliot";
+var nome = "Eliot2";
+
+let nome = "Eliot";
+let nome = "Eliot2";
+
+
---
+++ Seção 2 - JavaScript Básico - Para iniciantes/19. Tipos de dados primitivos.js
@@ -0,0 +1,20 @@
+let nome = "Eliot"; //Tipo string
+let idade = 30; // Tipo number
+let altura = 1.80; // Tipo number
+let solteiro = true; //Tipo boolean
+var semIniciar; //Tipo undefined Não aponta para nenhum valor
+let valorNull = null; //Tipo object que não aponta para nenhum valor
+let valorUndefined = undefined; //Tipo object que não aponta para nenhum valor
+let simbolo = Symbol(); //Tipo symbol
+let novoObjeto = {}; //Tipo object
+let novoArray = []; //Tipo object
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numeros.forEach((n) => {
+    if(n == 2){
+        console.log("Aqui e o 2");
+        return;
+    }
+    console.log(n);
+});

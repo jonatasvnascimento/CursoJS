d1adf0f5bde30e47fa006e442e78f05b2099e697
Jonatas Viana <jonatas.viana@kria.com.br>
2024-04-18 14:59:45 -0300

    12 - 14

---
+++ Seção 2 - JavaScript Básico - Para iniciantes/13. Variáveis com let.js
@@ -0,0 +1,20 @@
+let a = "a";
+let b = 0;
+let c = 'A'
+let d = undefined
+
+// console.log(a, b, c, d)
+
+console.log(typeof d)
+
+
+let nome = "João";
+let nascimento = 2000;
+let casamento = 2012;
+let esposa = "Maria";
+let filho = "Eduardo"
+
+// console.log(`${nome} nasceu em ${nascimento}`)
+// console.log(`${nome} se casou com ${esposa} em ${casamento}`)
+// console.log(`${nome} teve seu primeiro filho ${filho} em ${casamento}`)
+
